f6bd41d4383d208063782cd0b38022e6e8dc28bb
Luka Budagov <106598468+budagovi@users.noreply.github.com>
2025-02-27 00:34:47 +0400

    FIX: import typo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import ThemeRegistry from "./lib/theme/ThemeRegistry";
+import ThemeRegistry from "../lib/theme/ThemeRegistry";
 
 export const metadata: Metadata = {
   title: {
     default: "Aurora Plants",
     template: "Aurora Plants - %s",
   },
-  description: "Online store for indoor and outdoor plants",
+  description: "Online store for  indoor and outdoor plants",
 };
 
 export default function RootLayout({
